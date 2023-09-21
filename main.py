fefd61bb471330e2b8762d5b9b2c4481fc2a2bb6
Billyx15 <139943139+Billyx15@users.noreply.github.com>
2023-09-21 17:41:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-class BankAccount:
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
 
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      # self.__account_balance = self.__account_balance+amount
-      print("Deposited â‚¹",amount,". New balance: â‚¹",self.__account_balance)
-    else:
-      print("Invalid deposit amount.")
+  return indices
 
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      # self.__account_balance = self.__account_balance - amount
-      print("Withdrew â‚¹",amount,". New balance: â‚¹",self.__account_balance)
-    else:
-      print("Invalid withdrawal amount or insufficient balance.")
 
-  def display_balance(self):
-    print("Account balance for ",self.__account_holder_name,"(Account #",self.__account_number,"): â‚¹",self.__account_balance)
-
-account = BankAccount(account_number="123456789",account_holder_name="Ameen Ahsan",initial_balance=2000.0)
-
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-account.display_balance()
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
